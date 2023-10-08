5187b5c4d113c3103dc15408c34ca59c53cdb46f
BadStructure <84621744+BadStructure@users.noreply.github.com>
2023-10-08 15:20:52 +0800

    1. recommendation features (1) connect to db (2) route for userSub (3) route for recommendation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # main.py
 from flask import Flask, request, jsonify, redirect, render_template, session, url_for
 from algoliasearch.search_client import SearchClient
+from algoliasearch.recommend_client import RecommendClient
 
 # Auth0: Import
 import json
@@ -9,12 +10,19 @@ from urllib.parse import quote_plus, urlencode
 from authlib.integrations.flask_client import OAuth
 from dotenv import find_dotenv, load_dotenv
 
+# Project DataBase
+import db
+import util
+import threading
+
 
 # Flask
+db.InitDB()
 app = Flask(__name__)
 
 
 # Algolia
+recommendClient = RecommendClient.create("SF0IKHXEOM", "fad1aa330a1a4c92f332df2159732ea3")
 client = SearchClient.create('SF0IKHXEOM', '68ff5251ad7e59a3da88bd09b7994d53')
 index = client.init_index('test_arXiv')
 
@@ -54,10 +62,55 @@ def home():
 def detail(object_id):
     paper = index.get_object(object_id)  # Retrieve the paper data from Algolia based on the objectID
 
+    # Get user ID, if logged in, record access records.
+    user = session.get("user")
+    userID = util.getUserID(user)
+
+    if userID is not None:
+        # db.SaveRecordReadingHistory(1, userID, object_id)
+        thread = threading.Thread(target=db.SaveRecordReadingHistory, args=(1, userID, object_id))
+        thread.start()
+
     if paper:
         return render_template('detail.html', paper=paper, session=session.get("user"))
     else:
         return "Paper not found", 404  # Handle the case where the paper is not found
+
+
+# user like this paper
+@app.route("/detail/<object_id>/like", methods=['GET'])
+def like(object_id):
+    # Get user ID, if logged in, record access records.
+    user = session.get("user")
+    userID = util.getUserID(user)
+
+    if userID is not None:
+        # db.SaveRecordReadingHistory(2, userID, object_id)
+        thread = threading.Thread(target=db.SaveRecordReadingHistory, args=(2, userID, object_id))
+        thread.start()
+
+    return "ok", 200
+
+
+# get userSub hash
+@app.route("/sub", methods=['GET'])
+def getSub():
+    user = session.get("user")
+    userID = util.getUserID(user)
+    if userID is not None:
+        return userID, 200
+    else:
+        return "", 200
+
+
+# get recommendData
+@app.route("/recommend", methods=['GET'])
+def getRecommendData():
+    userID = request.args.get("userID")
+
+    recommendData = db.GetRecommendData(userID)
+
+    return recommendData, 200
 
 
 # Auth0: Call back
